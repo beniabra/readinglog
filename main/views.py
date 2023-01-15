53eb882f1159b33a8f4241671556e80769e35f45
beniabra <beniabra@udel.edu>
2023-01-15 14:00:39 -0500

    Created base.html and home.html

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(response):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(response, "main/home.html", {})
