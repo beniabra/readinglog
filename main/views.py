c7bd783bf9509dab32d8afabccfdabe4cf386325
beniabra <beniabra@udel.edu>
2023-01-17 11:19:15 -0500

    Finished base template and created pages

--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def home(response):
     return render(response, "main/home.html", {})
+
+def books(response):
+    return render(response, "main/books.html", {})
+
+def stats(response):
+    return render(response, "main/stats.html", {})
