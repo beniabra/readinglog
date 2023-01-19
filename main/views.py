a270599f075f89f39630804668ffe99011c61cb4
beniabra <beniabra@udel.edu>
2023-01-19 13:05:11 -0500

    Added progress bar, experiment with API

--- main/views.py
+++ main/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 def home(response):
-    return render(response, "main/home.html", {})
+    url = 'https://www.googleapis.com/books/v1/volumes/lxGuDwAAQBAJ'
+    book = requests.get(url).json()
+    book_title = book["volumeInfo"]["title"]
+    book_author = book["volumeInfo"]["authors"][0]
+    results = {
+        "books_finished": 5,
+        "book_goal": 35,
+        "percent": round(5/35*100),
+        "book_title": book_title,
+        "book_author": book_author
+    }
+    return render(response, "main/home.html", results)
 
 def books(response):
     return render(response, "main/books.html", {})
